0a423a2b81e78f313dd5966b04bc1a39e1280ea0
otmane aatik <aaotmane@gmail.com>
2021-03-11 17:49:20 -0500

    Router

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
-function RenderDirectoryItem({campsite, onClick}){
+function RenderDirectoryItem({campsite}){
   return (
-    <Card onClick={() => onClick(campsite.id)}>
+    <Card>
       <CardImg width="100%" src={campsite.image} alt={campsite.name} />
       <CardImgOverlay>
         <CardTitle>{campsite.name}</CardTitle>
@@ -17,7 +17,7 @@ function Directory({campsites, onClick}){
     const directory = campsites.map((campsite) => {
       return (
         <div key={campsite.id} className="col-md-5 m-1">
-          <RenderDirectoryItem campsite={campsite} onClick={onClick}/>
+          <RenderDirectoryItem campsite={campsite}/>
         </div>
       );
     });
